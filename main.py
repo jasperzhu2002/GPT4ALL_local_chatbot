8ce1ba13ae7c71e13e77fe122e1b836704c6c711
jasperzhu2002 <jasperzhu2002@gmail.com>
2023-08-02 10:19:33 +0800

    custom prompt no memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import GPT4All
@@ -12,18 +12,24 @@ from langchain import PromptTemplate, LLMChain
 files = ["review"]
 db = loadPDFs(files)
 
+template = """Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+Use three sentences maximum and keep the answer as concise as possible. 
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
 llm = GPT4All(
-    model="./1ggml-gpt4all-j-v1.3-groovy.bin",
+    model="./1ggml-model-gpt4all-falcon-q4_0.bin",
     backend="gptj",
     verbose=False
 )
 
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-qa = ConversationalRetrievalChain.from_llm(
+qa = RetrievalQA.from_chain_type(
     llm,
-    db.as_retriever(search_kwargs={"k": 3}),
-    memory=memory,
+    retriever=db.as_retriever(search_kwargs={"k": 3}),
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
 )
 
 chat_history = []
@@ -31,60 +37,7 @@ while True:
     query = input('Prompt: ')
     if query == "quit":
         sys.exit()
-    result = qa({'question': query})
-    print('Answer: ' + result['answer'])
+    result = qa({'query': query})
+    print('Answer: ' + result['result'])
     
     # chat_history.append((query, result['answer']))
-
-
-
-
-# import sys
-# from langchain.chains import RetrievalQA
-
-# from langchain.document_loaders import PyPDFLoader
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain.llms import GPT4All
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.vectorstores import Chroma
-# from pdf import loadPDFs
-# from langchain.memory import ConversationBufferMemory
-# from langchain import PromptTemplate, LLMChain
-
-# files = ["review"]
-# db = loadPDFs(files)
-
-# llm = GPT4All(
-#     model="./1ggml-model-gpt4all-falcon-q4_0.bin",
-#     backend="gptj",
-#     verbose=False
-# )
-
-# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-# template = """Use the following pieces of context to answer the question at the end. 
-# If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-# Use three sentences maximum and keep the answer as concise as possible. 
-# Always say "thanks for asking!" at the end of the answer. 
-# {context}
-# Question: {question}
-# Helpful Answer:"""
-# QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
-
-# # qa = ConversationalRetrievalChain.from_llm(
-# qa = RetrievalQA.from_chain_type(
-#     llm,
-#     retriever=db.as_retriever(search_kwargs={"k": 3}),
-#     # memory=memory,
-#     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
-# )
-
-# chat_history = []
-# while True:
-#     query = input('Prompt: ')
-#     if query == "quit":
-#         sys.exit()
-#     result = qa({'query': query})
-#     print('Answer: ' + result['result'])
-    
-#     # chat_history.append((query, result['answer']))
